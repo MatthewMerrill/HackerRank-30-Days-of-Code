51ab993d9aee2fea3e086b3c1594aa9fe2ab917c
MatthewMerrill <mattmerr47@gmail.com>
2016-01-17 00:29:39 -0800

    Create  Day0-PrintHelloWorld.cpp

---
+++ Cpp/ Day0-PrintHelloWorld.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// https://www.hackerrank.com/contests/30-days-of-code/challenges/day-0-print-hello-world/submissions/code/4670326
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    cout << "Hello World." << endl << "Welcome to 30 Days of Code.";
+    
+    return 0;
+}
