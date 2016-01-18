b6b9ea7cbdb607c8cb4333b6337ea6a8af37387f
MatthewMerrill <mattmerr47@gmail.com>
2016-01-17 19:43:02 -0800

    Create Day0-PrintHelloWorld.java

---
+++ Java8/Day0-PrintHelloWorld.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class HelloWorld {
+
+// https://www.hackerrank.com/contests/30-days-of-code/challenges/day-0-print-hello-world/submissions/code/4681442
+
+       public static void main(String[] args) {
+           System.out.println("Hello World.");
+           System.out.println("Welcome to 30 Days of Code.");
+       }
+   }
+
+
