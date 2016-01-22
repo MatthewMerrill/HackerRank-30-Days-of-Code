a8356142f2e39ecbf2d92121cea6d88832704633
MatthewMerrill <mattmerr47@gmail.com>
2016-01-22 15:13:23 -0800

    Create Day21-Generics.java

---
+++ Java8/Day21-Generics.java
@@ -0,0 +1,11 @@
+
+// https://www.hackerrank.com/contests/30-days-of-code/challenges/day-21-generics/submissions/code/4760259
+
+class Printer
+{
+    public void printArray(Object[] objects) {
+        for (Object obj : objects) {
+            System.out.println(obj);
+        }
+    }
+}
